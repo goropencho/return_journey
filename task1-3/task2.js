e1c9ae1b2e9596d3566ae016c494d357d46399a1
Gaurav Panchal <gkp.125@gmail.com>
2023-12-24 00:23:28 +0530

    Task2 Done

--- task1-3/task2.js
+++ task1-3/task2.js
@@ -10,7 +10,16 @@ const object2 = {
 }
 
 function mergeObjectV1(object1,object2){
-
+    let result = object1
+    for(let i of Object.keys(object2)){
+        if(!result[i]){
+            result[i] = object2[i];
+        }else{
+            value = result[i]
+            result[i] = [value,object2[i]];
+        }
+    }
+    return result
 }
 
 function mergeObjectV2(object1, object2){
@@ -25,5 +34,5 @@ function mergeObjectV2(object1, object2){
     return result;
 }
 
-
+console.log(mergeObjectV1(object1,object2))
 console.log(mergeObjectV2(object1,object2))
